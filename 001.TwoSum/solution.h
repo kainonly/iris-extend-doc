99b223068d719e0b1d81a6aa329c916c97b87147
kain <zhangtqx@vip.qq.com>
2018-10-24 23:35:41 +0800

    Submit:fixed leetcode

---
+++ 001.TwoSum/solution.h
@@ -0,0 +1,16 @@
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+
+    }
+};
+
+#endif //LEETCODE_SOLUTION_H
---
+++ 002.AddTwoNumber/solution.h
@@ -0,0 +1,22 @@
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <iostream>
+
+using namespace std;
+
+//struct ListNode {
+//    int val;
+//    ListNode *next;
+//
+//    ListNode(int x) : val(x), next(NULL) {}
+//};
+//
+//class Solution {
+//public:
+//    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+//
+//    }
+//};
+
+#endif //LEETCODE_SOLUTION_H
