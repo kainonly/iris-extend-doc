1c4d0eb9d20df91c9a288cb0cab278f36e1d99ca
kain <zhangtqx@vip.qq.com>
2018-10-25 01:30:10 +0800

    Submit:Two Sum

--- 001.TwoSum/main.cpp
+++ 001.TwoSum/main.cpp
@@ -1,6 +1,14 @@
 #include "solution.h"
 
 int main() {
-
-
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+//    vector<int> nums = {3, 3};
+//    int target = 6;
+    auto solution = Solution();
+    vector<int> result = solution.twoSum(nums, target);
+    for (int x:result) {
+        cout << x << endl;
+    }
+    return 0;
 }
--- 001.TwoSum/solution.h
+++ 001.TwoSum/solution.h
@@ -3,13 +3,38 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int> &nums, int target) {
+        vector<int> result;
+        unordered_map<int, int> map = {};
+        unordered_map<int, int> next = {};
+        int len = nums.size();
+        for (int i = 0; i < len; ++i) {
+            int x = nums[i];
+            auto search = map.find(x);
+            if (search != map.end()) next.insert({x, i});
+            else map.insert({x, i});
+        }
 
+        for (int i = 0; i < len; ++i) {
+            int x = nums[i];
+            int y = target - x;
+            auto search_map = map.find(y);
+            if (search_map != map.end() && map[y] != i) {
+                result = {i, map[y]};
+                break;
+            } else {
+                auto search_next = next.find(y);
+                if (search_next == next.end()) continue;
+                result = {i, next[y]};
+            };
+        }
+        return result;
     }
 };
 
