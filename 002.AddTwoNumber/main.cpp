eef080b4ecd686c857a57ec3da5bf92649794224
kain <zhangtqx@vip.qq.com>
2018-10-26 00:10:13 +0800

    Submit:Add Two Number

--- 002.AddTwoNumber/main.cpp
+++ 002.AddTwoNumber/main.cpp
@@ -1,6 +1,26 @@
 #include "solution.h"
+#include <forward_list>
+
+
+ListNode ForWardListToListNode(forward_list<int> list) {
+    auto begin = list.begin();
+    auto *l1 = (list.empty()) ? nullptr : new ListNode(*begin++);
+    for (ListNode *x = l1; begin != list.end(); x = x->next) {
+        x->next = new ListNode(*begin++);
+    }
+    return *l1;
+}
+
 
 int main() {
+    ListNode l1 = ForWardListToListNode({2, 4, 3});
+    ListNode l2 = ForWardListToListNode({5, 6, 4});
+    ListNode p = l1;
+    while (p.next) {
+        cout << p.val << endl;
+        p = *p.next;
+    }
+
 
     return 0;
 }
--- 002.AddTwoNumber/solution.h
+++ 002.AddTwoNumber/solution.h
@@ -5,18 +5,21 @@
 
 using namespace std;
 
-//struct ListNode {
-//    int val;
-//    ListNode *next;
-//
-//    ListNode(int x) : val(x), next(NULL) {}
-//};
-//
-//class Solution {
-//public:
-//    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-//
-//    }
-//};
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        ListNode *result = nullptr;
+
+
+        return result;
+    }
+};
 
 #endif //LEETCODE_SOLUTION_H
