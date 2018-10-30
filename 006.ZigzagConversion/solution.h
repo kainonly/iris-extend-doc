5f9b2f6842bfded808798226c87217d1c8d5f09f
kain <zhangtqx@vip.qq.com>
2018-10-30 21:19:14 +0800

    Update:progress

---
+++ 006.ZigzagConversion/solution.h
@@ -0,0 +1,17 @@
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string str;
+        return str;
+    }
+};
+
+#endif //LEETCODE_SOLUTION_H
