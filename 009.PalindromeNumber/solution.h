e9addb349a6a10aa2581e7623ab3f7a7e7e80626
kain <zhangtqx@vip.qq.com>
2018-10-30 22:05:54 +0800

    Submit:PalindromeNumber

--- 009.PalindromeNumber/solution.h
+++ 009.PalindromeNumber/solution.h
@@ -8,7 +8,15 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(int x) {
-
+        if (x < 0) return false;
+        int result = 0;
+        while (x != 0) {
+            int last = x % 10;
+            x = (x - last) / 10;
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && last > 7)) return false;
+            result = result * 10 + last;
+        }
+        return x == result;
     }
 };
 
