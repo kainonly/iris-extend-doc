40667efc08cdef75ec14f948705fe97487bcb5eb
kain <zhangtqx@vip.qq.com>
2018-10-30 21:47:34 +0800

    Submit:Reverselnteger

--- 007.ReverseInteger/main.cpp
+++ 007.ReverseInteger/main.cpp
@@ -1,7 +1,9 @@
 #include "solution.h"
 
 int main() {
-    int result = Solution().reverse(123);
+//    int result = Solution().reverse(123);
+//    int result = Solution().reverse(-123);
+    int result = Solution().reverse(120);
     cout << result << endl;
     return 0;
 }
