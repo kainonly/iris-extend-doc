5a48c98caa18e54029fadc7398faac77f6eb0d9e
kain <zhangtqx@vip.qq.com>
2018-10-30 21:53:56 +0800

    Submit:Reverselnteger

--- 007.ReverseInteger/solution.h
+++ 007.ReverseInteger/solution.h
@@ -12,6 +12,8 @@ public:
         while (x != 0) {
             int last = x % 10;
             x = (x - last) / 10;
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && last > 7)) return 0;
+            if (result < INT_MIN / 10 || (result == INT_MIN / 10 && last < -8)) return 0;
             result = result * 10 + last;
         }
         return result;
