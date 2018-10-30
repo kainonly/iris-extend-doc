33b4251a7482cc69004db76a4710693ae9ce04b1
kain <zhangtqx@vip.qq.com>
2018-10-30 21:13:21 +0800

    Update:progress

---
+++ 004.MedianOfTwoSortedArrays/solution.h
@@ -0,0 +1,17 @@
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        double x = 0;
+        return x;
+    }
+};
+
+#endif //LEETCODE_SOLUTION_H
