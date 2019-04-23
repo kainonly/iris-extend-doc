7e681a565ebee37bc1e4ead2d8a2ca8f6ff00348
kain <zhangtqx@vip.qq.com>
2019-04-23 23:41:36 +0800

    Curd studly url -> Controller

--- src/facade/Curd.php
+++ src/facade/Curd.php
@@ -5,6 +5,7 @@ namespace lumen\bit\facade;
 
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /**
  * Class Curd
@@ -24,12 +25,12 @@ class Curd extends Facade
             'prefix' => $name,
             'middleware' => $middleware
         ], function () use ($name, $traits) {
-            if (in_array('get', $traits)) Route::post('get', ucfirst($name) . '@get');
-            if (in_array('originLists', $traits)) Route::post('originLists', ucfirst($name) . '@originLists');
-            if (in_array('lists', $traits)) Route::post('lists', ucfirst($name) . '@lists');
-            if (in_array('add', $traits)) Route::post('add', ucfirst($name) . '@add');
-            if (in_array('edit', $traits)) Route::post('edit', ucfirst($name) . '@edit');
-            if (in_array('delete', $traits)) Route::post('delete', ucfirst($name) . '@delete');
+            if (in_array('get', $traits)) Route::post('get', Str::studly($name) . '@get');
+            if (in_array('originLists', $traits)) Route::post('originLists', Str::studly($name) . '@originLists');
+            if (in_array('lists', $traits)) Route::post('lists', Str::studly($name) . '@lists');
+            if (in_array('add', $traits)) Route::post('add', Str::studly($name) . '@add');
+            if (in_array('edit', $traits)) Route::post('edit', Str::studly($name) . '@edit');
+            if (in_array('delete', $traits)) Route::post('delete', Str::studly($name) . '@delete');
         });
     }
 }
