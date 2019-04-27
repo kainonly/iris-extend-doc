0d5e2e6ac8dab7f60f0ff2f770b3c739f637cda9
kain <zhangtqx@vip.qq.com>
2019-04-27 22:45:44 +0800

    Update CorsCookieAuth

--- src/common/CorsCookieAuth.php
+++ src/common/CorsCookieAuth.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Facade;
 use Lcobucci\JWT\Builder;
 use Lcobucci\JWT\Parser;
 use laravel\bit\redisModel\RefreshToken;
+use Lcobucci\JWT\Token;
 
 /**
  * Class CorsCookieAuth
@@ -21,108 +22,72 @@ final class CorsCookieAuth extends Facade
      * @param string $userId User ID
      * @param string $roleId Role ID
      * @param array $symbol Symbol Tag
-     * @return array
+     * @return boolean
      */
     public static function setToken($label, $userId, $roleId, $symbol = [])
     {
-        try {
-            $config = Config::get('auth.cors_cookie_auth');
-            $jti = Ext::uuid();
-            $ack = Ext::random();
-            $token = (new Builder())
-                ->setId($jti)
-                ->setIssuer($config['label'][$label]['issuer'])
-                ->setAudience($config['label'][$label]['audience'])
-                ->set('ack', $ack)
-                ->set('user', $userId)
-                ->set('role', $roleId)
-                ->set('symbol', $symbol)
-                ->setExpiration(time() + $config['ttl'])
-                ->sign($config['sha256'], $config['secret'])
-                ->getToken();
-
-            $result = RefreshToken::factory($jti, $ack);
-            if (!$result) return [
-                'error' => 1,
-                'msg' => 'error:factory_failed'
-            ];
-
+        $config = Config::get('auth.cors_cookie_auth');
+        $jti = Ext::uuid();
+        $ack = Ext::random();
+        $token = (new Builder())
+            ->setId($jti)
+            ->setIssuer($config['label'][$label]['issuer'])
+            ->setAudience($config['label'][$label]['audience'])
+            ->set('ack', $ack)
+            ->set('user', $userId)
+            ->set('role', $roleId)
+            ->set('symbol', $symbol)
+            ->setExpiration(time() + $config['ttl'])
+            ->sign($config['sha256'], $config['secret'])
+            ->getToken();
+        $result = RefreshToken::factory($jti, $ack);
+        if (!$result) {
+            return false;
+        } else {
             Cookie::queue($label, (string)$token);
-            return [
-                'error' => 0,
-                'msg' => 'ok'
-            ];
-        } catch (\Exception $e) {
-            return [
-                'error' => 1,
-                'msg' => 'error:' . $e->getMessage()
-            ];
+            return true;
         }
     }
 
     /**
      * Token Verify
      * @param string $label
-     * @return array
+     * @return boolean|Token
      */
     public static function tokenVerify($label)
     {
-        try {
-            $config = Config::get('auth.cors_cookie_auth');
-            if (!Cookie::get($label)) return [
-                'error' => 1,
-                'msg' => 'error:not_exists_token'
-            ];
+        $config = Config::get('auth.cors_cookie_auth');
+        if (!Cookie::get($label)) return false;
 
-            $token = (new Parser())->parse(Cookie::get($label));
-            if (!$token->verify($config['sha256'], $config['secret'])) return [
-                'error' => 1,
-                'msg' => 'error:verify'
-            ];
+        $token = (new Parser())->parse(Cookie::get($label));
+        if (!$token->verify($config['sha256'], $config['secret'])) return false;
 
-            if ($token->getClaim('iss') != $config['label'][$label]['issuer'] ||
-                $token->getClaim('aud') != $config['label'][$label]['audience']) return [
-                'error' => 1,
-                'msg' => 'error:incorrect'
-            ];
+        if ($token->getClaim('iss') != $config['label'][$label]['issuer'] ||
+            $token->getClaim('aud') != $config['label'][$label]['audience']) return false;
 
-            if ($token->isExpired()) {
-                $result = (new RefreshToken())->verify(
-                    $token->getClaim('jti'),
-                    $token->getClaim('ack')
-                );
+        if ($token->isExpired()) {
+            $result = (new RefreshToken())->verify(
+                $token->getClaim('jti'),
+                $token->getClaim('ack')
+            );
+            if (!$result) return false;
+            $newToken = (new Builder())
+                ->setId($token->getClaim('jti'))
+                ->setIssuer($config['label'][$label]['issuer'])
+                ->setAudience($config['label'][$label]['audience'])
+                ->set('ack', $token->getClaim('ack'))
+                ->set('user', $token->getClaim('user'))
+                ->set('role', $token->getClaim('role'))
+                ->set('symbol', $token->getClaim('symbol'))
+                ->setExpiration(time() + $config['ttl'])
+                ->sign($config['sha256'], $config['secret'])
+                ->getToken();
 
-                if (!$result) return [
-                    'error' => 1,
-                    'msg' => 'error:expired'
-                ];
-
-                $newToken = (new Builder())
-                    ->setId($token->getClaim('jti'))
-                    ->setIssuer($config['label'][$label]['issuer'])
-                    ->setAudience($config['label'][$label]['audience'])
-                    ->set('ack', $token->getClaim('ack'))
-                    ->set('user', $token->getClaim('user'))
-                    ->set('role', $token->getClaim('role'))
-                    ->set('symbol', $token->getClaim('symbol'))
-                    ->setExpiration(time() + $config['ttl'])
-                    ->sign($config['sha256'], $config['secret'])
-                    ->getToken();
-
-                $token = $newToken;
-                Cookie::queue($label, (string)$token);
-            }
-
-            return [
-                'error' => 0,
-                'msg' => 'ok'
-            ];
-        } catch (\Exception $e) {
-            return [
-                'error' => 1,
-                'msg' => 'error:' . $e->getMessage()
-            ];
+            $token = $newToken;
+            Cookie::queue($label, (string)$token);
         }
+
+        return $token;
     }
 
     /**
